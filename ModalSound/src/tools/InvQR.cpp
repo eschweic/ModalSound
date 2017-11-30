4b7df978e7c3791a672822e6d1fb535d06a758a4
eschweic <eston.schweickart@gmail.com>
2017-11-30 15:12:53 -0500

    Matrices store lower half, not upper.

--- ModalSound/src/tools/InvQR.cpp
+++ ModalSound/src/tools/InvQR.cpp
@@ -3,12 +3,14 @@
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <chrono>
 
 #include <boost/program_options.hpp>
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <Eigen/Sparse>
+#include <Eigen/Eigenvalues>
 
 #include "utils/term_msg.h"
 #include "geometry/FixedVtxTetMesh.hpp"
@@ -93,12 +95,6 @@ static void parse_cmd(int argc, const char* argv[])
         PRINT_ERROR("Density value must be positive [d=%g now]\n", density);
         exit(1);
     }
-    
-    // if ( freqLow >= freqHigh )
-    // {
-    //     PRINT_ERROR("Frequency lower bound must be smaller than the upper bound\n");
-    //     exit(1);
-    // }
 
     // if ( outFile.empty() ) 
     // {
@@ -122,7 +118,7 @@ static void parse_cmd(int argc, const char* argv[])
 }
 
 /*
- * The matrix file only stores the upper triangle part
+ * The matrix file only stores the lower triangle part
  */
 static uint8_t read_csc_dmatrix(const char* file, 
   std::vector<int>& ptrrow,
@@ -208,8 +204,8 @@ Eigen::MatrixXd getNullspace(const FixedVtxTetMesh<double>& tmesh) {
 struct SparseData {
 protected:
   int nrow, ncol;
-  std::vector<int> innerPtr;
   std::vector<int> outerPtr;
+  std::vector<int> innerPtr;
   std::vector<double> data;
   Eigen::Map<Eigen::SparseMatrix<double>> map;
 
@@ -217,6 +213,12 @@ public:
   SparseData(const char* file) : map(0, 0, 0, nullptr, nullptr, nullptr) {
     uint8_t result = read_csc_dmatrix(file, outerPtr, innerPtr, data, nrow, ncol);
     if (result == 255) throw;
+    for (int i  = 0; i  < outerPtr.size(); i++) {
+      outerPtr[i]--;
+    }
+    for (int i=0; i < innerPtr.size(); i++) {
+      innerPtr[i]--;
+    }
     map = Eigen::Map<Eigen::SparseMatrix<double>>(nrow, ncol, data.size(), outerPtr.data(), innerPtr.data(), data.data());
   }
 
@@ -255,7 +257,7 @@ public:
   }
 
 
-  SparsePlusLowRank(const SparseData& M, const SparseData& K, const Eigen::MatrixXd& R) : M(M), K(K), R(R), cg(M.getMap()) {
+  SparsePlusLowRank(const SparseData& M, const SparseData& K, const Eigen::MatrixXd& R) : M(M), K(K), R(R), Minv(M.getMap()) {
     if (M.getMap().rows() != K.getMap().rows() || M.getMap().cols() != K.getMap().cols()) {
       PRINT_ERROR("The size of M and K must match");
       throw;
@@ -269,7 +271,7 @@ public:
   const SparseData& M;
   const SparseData& K;
   const Eigen::MatrixXd& R;
-  Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Upper> cg;
+  Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower, Eigen::IncompleteCholesky<double, Eigen::Lower>> Minv;
 };
 
 namespace Eigen {
@@ -282,7 +284,7 @@ namespace Eigen {
       template<typename Dest>
       static void scaleAndAddTo(Dest& dst, const SparsePlusLowRank& lhs, const Rhs& rhs, const Scalar& alpha) {
         // This method implements "dst += alpha * lhs * rhs" inplace
-        dst.noalias() += alpha * lhs.cg.solve(lhs.K.getMap().selfadjointView<Eigen::Upper>() * rhs);
+        dst.noalias() += alpha * lhs.Minv.solve(lhs.K.getMap().selfadjointView<Eigen::Lower>() * rhs);
         dst.noalias() += alpha * lhs.R * (lhs.R.transpose() * rhs);
       }
     };
@@ -295,33 +297,32 @@ Eigen::VectorXd invQR(const SparsePlusLowRank& A, int k) {
   int n = A.rows();
   int maxIters = 20;
 
-  std::default_random_engine generator;
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
   std::normal_distribution<double> dist(0.0, 1.0);
   auto normal = [&] () { return dist(generator); };
   Eigen::MatrixXd Z = Eigen::MatrixXd::NullaryExpr(n, k, normal);
   Eigen::MatrixXd Y(n, k);
   Eigen::VectorXd S(k);
   SPLRSolver solver(A);
-  solver.setMaxIterations(10);
 
   for (int iter=0; iter<maxIters; iter++) {
     for (int i=0; i<k; i++) {
       Y.col(i) = solver.solve(Z.col(i));
-      std::cout << "splr solve" << std::endl;
     }
-    std::cout << "start Qr" << std::endl;
     Z = Y.colPivHouseholderQr().matrixQ();
-    std::cout << "Qr done" << std::endl;
 
     for (int i=0; i<k; i++) {
-      S(i) = Z.col(i).dot(A.K.getMap().selfadjointView<Eigen::Upper>() * Z.col(i)) /
-      Z.col(i).dot(A.M.getMap().selfadjointView<Eigen::Upper>() * Z.col(i));
+      S(i) = Z.col(i).dot(A.K.getMap().selfadjointView<Eigen::Lower>() * Z.col(i)) /
+      Z.col(i).dot(A.M.getMap().selfadjointView<Eigen::Lower>() * Z.col(i));
     }
-    std::cout << "iter " << iter << ": " << S.transpose() << std::endl;
   }
 
-  return S;
+  return S.cwiseInverse();
 }
+
+
+// #include "io/TetMeshWriter.hpp"
 
 int main(int argc, char const *argv[]) {
 
@@ -336,7 +337,32 @@ int main(int argc, char const *argv[]) {
   SparseData K(stiffMFile.c_str());
   SparsePlusLowRank splr(M, K, R);
 
+  // Verify MinvK + RR^T is nonsingular
+  // Eigen::MatrixXd A(R.rows(), R.rows());
+  // for (int i=0; i<A.cols(); i++) {
+  //   A.col(i).noalias() = splr * Eigen::VectorXd::Unit(A.cols(), i);
+  // }
+  // std::cout << "rank: " << A.colPivHouseholderQr().rank() << " / " << R.rows() << std::endl;
+  // std::cout << "A * R norm: " << (A * R).norm() << std::endl;
+  // for (int i=0; i<6; i++) {
+  //   std::cout << "K * R(" << i << ") norm: " << (K.getMap().selfadjointView<Eigen::Lower>() * R.col(i)).norm() << std::endl;
+  // }
+
   std::cout << invQR(splr, 10) << std::endl;
+
+  // Eigen::MatrixXd Md = M.getMap().toDense();
+  // Md = Md.selfadjointView<Eigen::Lower>();
+  // std::cout << "M:\n" << Md << "\n\n";
+  // std::cout << "M rank: " << Md.colPivHouseholderQr().rank() << " / " << R.rows() << std::endl;
+
+  // FixedVtxTetMesh<double> mesh;
+  // mesh.add_vertex(Point3d(0.0, 0.0, 0.0));
+  // mesh.add_vertex(Point3d(1.0, 0.0, 0.0));
+  // mesh.add_vertex(Point3d(0.0, 1.0, 0.0));
+  // mesh.add_vertex(Point3d(0.0, 0.0, 1.0));
+  // mesh.add_tet(0, 1, 2, 3);
+  // mesh.init();
+  // FV_TetMeshWriter_Double::write_mesh(argv[1], mesh);
 
   return 0;
 }
